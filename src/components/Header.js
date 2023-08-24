1bbe1a4d25ba51b301f04b05fc02b03e0c74bc9f
farrukh-Zain <121316072+farrukh-Zain@users.noreply.github.com>
2023-08-25 00:03:37 +0500

    I little changes on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,17 @@ function Header() {
   return (
     <header>
       <nav>
-        <div className="logo">Your Logo</div>
+        <div className="logo">
+        <img src="https://trello.com/assets/75d96a8b741cb5310663.png" alt=""/>
+          <div className='tri'>Trello</div>
+        </div>
         <ul className="nav-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Features</a></li>
-          <li><a href="#">Pricing</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="#">Workspaces</a></li>
+          <li><a href="#">Starred</a></li>
+          <li><a href="#">Templates</a></li>
+        
         </ul>
+        
         <button className="cta-button">Sign Up</button>
       </nav>
     </header>
